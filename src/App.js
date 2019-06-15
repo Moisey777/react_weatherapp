5cfeaa855a84868758b4289d1af1e52e267823ac
Rem Beliy <blog@rusinfoproduct.ru>
2019-06-15 15:35:07 +0300

    add api_key in the app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ const API_KEY = "f9bb1bcfaa06ae7ced9c234a1124356f"
 
 
 class App extends React.Component {
+	gettingWeather = async() => {
+		const api_url = await fetch(`api.openweathermap.org/data/2.5/weather?q=Kiev,ua&appid=${API_KEY}&units=metric`);
+		const data = await api_url.json();
+		consol.log();
+
+
+	}
+
+
   render () { 
 
     return (
