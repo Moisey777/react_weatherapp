d16fe22a14725615f6a3ed59cb87617e2b77a6e4
Rem Beliy <blog@rusinfoproduct.ru>
2019-06-17 19:52:16 +0300

    add temp sunset sunrise  city

--- src/App.js
+++ src/App.js
@@ -7,13 +7,51 @@ const API_KEY = "f9bb1bcfaa06ae7ced9c234a1124356f"
 
 
 class App extends React.Component {
+
+	state = {
+		temp: undefined,
+		city: undefined,
+		country: undefined,
+		sunrise: undefined,
+		sunset: undefined,
+		error: undefined
+
+	}
+
+
+
 	gettingWeather = async(e) => {
 		e.preventDefault();
 		const city = e.target.elements.city.value;
 		const api_url = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
 		const data = await api_url.json();
-		console.log(data);
+
+		if (city) 
+
+		const api_url = await fetch(`api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+		const data = await api_url.json();
+
+		var sunset = data.sys.sunset;
+		var date = new.Date();
+		date.setTime(sunset);
+		var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.genSeconds();
+
+
+
+
+		this.setState ({
+			temp: data.main.temp,
+			city: data.name,
+			country: data.sys.country,
+			sunrise: data.sys.sunrise,
+			sunset: sunset_date,
+			error: "" 
+
+
+		});
+
 	}
+}
 
 
   render () { 
@@ -25,7 +63,15 @@ class App extends React.Component {
     
     <Info />
     <Form weatherMethod = {this.gettingWeather}/>
-    <Weather />
+    <Weather
+    temp = {this.state.temp}
+    city = {this.state.city}
+    country = {this.state.country}
+    sunrise = {this.state.sunrise}
+    sunset = {this.state.sunset}
+    error = {this.state.error}
+
+    />
 
     </div>
     
--- src/components/weather.js
+++ src/components/weather.js
@@ -4,7 +4,16 @@ class Weather extends React.Component {
   render () { 
 
     return (
-    <div>Компонент с погодной информацией</div>   
+    	<div>
+    {this.props.city &&
+<div><p>Местоположение: {this.props.city}, {this.props.country}</p>
+     <p>Температура: {this.props.temp}</p>
+     <p>Восход солнца: {this.props.sunrise}</p>
+     <p>Закат солнца: {this.props.sunset}</p>
+    </div>
+    }
+    </div>
+
 
       );
 
